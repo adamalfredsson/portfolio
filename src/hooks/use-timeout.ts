ffc6b2e2e5eb0049ce21074a923907790062a008
Adam Alfredsson <alfredsson.adam@gmail.com>
2022-09-15 10:50:34 +0200

    Remove threejs

--- src/hooks/use-timeout.ts
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-
-export const useTimeout = (callback: () => void, delay: number) => {
-  useEffect(() => {
-    const timeout = setTimeout(callback, delay);
-    return () => clearTimeout(timeout);
-  }, []);
-};
